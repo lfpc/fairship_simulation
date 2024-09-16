540e35c39ae448ffa52374b5513a99ab7f2446a0
lfpc <luisfelipe1998@gmail.com>
2024-09-16 12:15:44 +0200

    generate_and send subsample file can be called from anywhere

--- samples/scripts/generate_and_send_pkl.sh
+++ samples/scripts/generate_and_send_pkl.sh
@@ -1,2 +1,2 @@
-python root_to_pkl.py
-scp ../subsample.pkl lprate@linux.physik.uzh.ch://home/hep/lprate/projects/MuonsAndMatter/data
+python $PROJECTS_DIR/fairship_simulation/samples/scripts/root_to_pkl.py
+scp $PROJECTS_DIR/fairship_simulation/samples/subsample.pkl lprate@linux.physik.uzh.ch://home/hep/lprate/projects/MuonsAndMatter/data

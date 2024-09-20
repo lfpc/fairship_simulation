21c33f23ba5a8ca3618d5aae751d2331adc09a28
lfpc <luisfelipe1998@gmail.com>
2024-09-20 12:13:12 +0200

    get full sample from all files

--- samples/scripts/get_full_sample.py
+++ samples/scripts/get_full_sample.py
@@ -6,7 +6,7 @@ import os
 
 def main(file_name):
     # Define the keys you want to extract
-    mc_track_keys = ['PdgCode', 'Px', 'Py', 'Pz', 'StartX', 'StartY', 'StartZ', 'W']
+    mc_track_keys = ['Px', 'Py', 'Pz', 'StartX', 'StartY', 'StartZ','PdgCode','W']
     columns = [f'MCTrack.f{key}' for key in mc_track_keys]
 
     data = []
@@ -24,12 +24,19 @@ def main(file_name):
 if __name__ == '__main__':
     input_dir = '/eos/experiment/ship/data/Mbias/background-prod-2018'
     output_dir = '/eos/experiment/ship/user/lcattela/SHIP/'
-    file_name = 'pythia8_Geant4_10.0_withCharmandBeauty0_mu.root'
-    output_name = 'full_sample.pkl'
-    data = main(os.path.join(input_dir,file_name))
-    print('DATA SHAPE:', data.shape)
-    with gzip.open(os.path.join(output_dir,output_name), "wb") as f:
-        pickle.dump(data, f)
+    output_name = 'full_sample_*.pkl'
+    #file_name = 'pythia8_Geant4_10.0_withCharmandBeauty0_mu.root'
+
+    start_str = 'pythia8_Geant4_10.0_withCharmandBeauty'
+    end_str = '_mu.root'
+    for file_name in os.listdir(input_dir):
+        if not file_name.startswith(start_str) and file_name.endswith(end_str): continue
+        num_file = file_name[len(start_str):-len(end_str)]
+        print(f'Checking file {num_file}')
+        data = main(os.path.join(input_dir,file_name))
+        print('DATA SHAPE:', data.shape)
+        with gzip.open(os.path.join(output_dir,output_name.replace('*',num_file)), "wb") as f:
+            pickle.dump(data, f)
 
 
 
